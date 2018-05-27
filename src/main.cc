891d92b3771561fb142958e6df6ef0f085fb6e47
Andrew Comminos <andrew@comminos.com>
2018-05-27 00:20:57 -0700

    Use llvm's command line argument parser for reading the input file.

--- src/main.cc
+++ src/main.cc
@@ -10,31 +10,57 @@
 
 // XXX(acomminos): just for printing IR
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CommandLine.h"
 
 int main(int argc, char* argv[]) {
-  if (argc == 1) {
-    std::cerr << "Usage: darlang [input-file]..." << std::endl;
-  }
+  llvm::cl::opt<std::string> input_file(llvm::cl::Positional, llvm::cl::desc("<input file>"), llvm::cl::init("-"));
+  llvm::cl::ParseCommandLineOptions(argc, argv);
 
   darlang::Logger logger(std::cerr);
-  for (int i = 1; i < argc; i++) {
-    const char* filename = argv[i];
 
-    std::ifstream is(filename);
-    darlang::Lexer l(logger, is, filename);
-    darlang::TokenStream ts(l);
-
-    darlang::Parser p(logger, ts);
-    auto module = p.ParseModule();
-
-    //darlang::ast::PrettyPrinter pp;
-    //module->Visit(pp);
-
-    darlang::typing::TypeTransform typer(logger);
-    auto types = typer.Reduce(*module);
-
-    llvm::LLVMContext llvm_context;
-    auto llvm_module = darlang::backend::LLVMModuleTransformer::Transform(llvm_context, types, *module);
-    llvm_module->print(llvm::errs(), nullptr);
+  // Read from stdin if '-' is specified as input.
+  std::istream* input;
+  if (input_file.compare("-") == 0) {
+    input = &std::cin;
+  } else {
+    input = new std::ifstream(input_file);
   }
+
+  if (!*input) {
+    std::cerr << "failed to open " << input_file << std::endl;
+    return 1;
+  }
+
+  darlang::Lexer l(logger, *input, input_file);
+  darlang::TokenStream ts(l);
+
+  darlang::Parser p(logger, ts);
+  auto module = p.ParseModule();
+
+  //darlang::ast::PrettyPrinter pp;
+  //module->Visit(pp);
+
+  darlang::typing::TypeTransform typer(logger);
+  auto types = typer.Reduce(*module);
+
+  llvm::LLVMContext llvm_context;
+  auto llvm_module = darlang::backend::LLVMModuleTransformer::Transform(llvm_context, types, *module);
+  llvm_module->print(llvm::errs(), nullptr);
+
+  /*
+  // XXX(acomminos): hackish object code generation
+  auto target_triple = llvm::sys::getDefaultTargetTriple();
+  std::string error;
+  auto target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+  if (!target) {
+    std::cerr << error << std::endl;
+    return 1;
+  }
+  auto target_machine = target->createTargetMachine(target_triple, "generic", "", llvm::Optional<llvm::Reloc::Model>());
+  llvm_module->setDataLayout(target_machine->createDataLayout());
+  llvm_module->setTargetTriple(target_triple);
+
+  std::string obj_filename = std::string(filename) + ".o";
+  */
+  delete input;
 }
